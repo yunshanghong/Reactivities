36906d9a438dbfb8e373c583b16d464bf47df7cf
yunshanghong <yunshanghong@gmail.com>
2019-11-20 21:07:53 +0800

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id{ get; set;}
+        public string Name { get; set;}
+    }
+}
